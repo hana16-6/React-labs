b9f19832bc917ed0f62aeb41d3918c7e1797d825
hana16-6 <64643374+hana16-6@users.noreply.github.com>
2021-10-25 02:15:15 +0200

    lab4

--- src/component/home/Home.jsx
+++ src/component/home/Home.jsx
@@ -1,5 +1,23 @@
 import React from "react";
+import myImg from "../../images/IMG_20191002_020213_548.jpg";
 
 export default function Home() {
-  return <h1>Home</h1>;
+  const style = {
+    borderRadius: "50%",
+  };
+  return (
+    <div className="container">
+      <div className="row justify-content-between align-items-center">
+        <div className="col-12 col-md-6 mt-5">
+          <img style={style} className="w-75" src={myImg} alt="profile" />
+        </div>
+        <h4 className="desc text-start col-12 col-md-6">
+          Hello I’m Hana Mohamed. I’m a front-end engineer, and I create fun,
+          innovative, accessible, and fast websites. I try to leave every bit of
+          code I touch more readable, modular, performant and accessible than I
+          found it.
+        </h4>
+      </div>
+    </div>
+  );
 }
